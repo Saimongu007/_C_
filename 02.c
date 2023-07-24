990e61e9a068b7ac1325493e303deb120287d39c
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-07-25 02:21:04 +0600

    Adult

---
+++ 02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+    int age;
+    scanf("%d",&age);
+    if(age>=18) {
+
+
+        printf("you are adult");
+    }
+    else {
+    printf("you are not adult");
+}
+}
